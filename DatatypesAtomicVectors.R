392878b2771455c24a54ce1f05d254881be25f5c
farias <gabrielfariaspaiva2004@gmail.com>
2026-04-09 22:22:38 -0300

    termino da aula de VA 1 e 2)

--- DatatypesAtomicVectors.R
+++ DatatypesAtomicVectors.R
@@ -41,4 +41,67 @@ typeof(x)
 
 #length
 length(x)
+typeof(x)
+
+#names
+z <- runif(6)
+
+#names are optional attribute not initially assigned
+names(z)
+print(z)
+
+# add names after variables is created
+names(z) <- c("ant","wasp","bee","humile","polistes",'bumble')
+print(z)
+
+# add names when variable is built (quotes are optional at this point)
+
+x <- c(gold=3, silver=5, lead=1)
+print(x)
+
+#reset names
+names(x) <- NULL
+print(x)
+
+#names can be added for only a few elements
+#names do not have to be distinct, but often are
+names(x) <- c("copper", "zinc")
+
+#using brackets to specify particular elements of an atomic vector
+y <- c(10,12,15)
+y[2] #specifies second element only
+y[c(1,3)]
+y[3] <-100
+print(y)
+
+#special elements in a vector
+
+# NA to indicate missing values (even at excel)
+y[3] <- NA
+typeof(z)
+length(z)
+typeof(z[3])
+
+z1 <- NA
+typeof(z1)
+
+is.na(y) # logical operator to find missing values
+mean(y) # will not work because of missing values
+!is.na(y) # will find elements that are not missing
+mean(!is.na(y)) # wrong because calculating on TRUE FALSE
+mean(y[!is.na(y)]) # correct way to eliminate NA
+
+#NaN, -Inf, and Inf from numeric division
+z1 <- 0/0
+print(z1)
+typeof(z1)
+z1 <- 1/0
+print(z1)
+typeof(z1)
+
+# NULL is an object that is nothing
+# a reserved word in R
+z <- NULL
+length(z)
+is.null(z)
 
